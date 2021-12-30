d95f1c14439f0ebf10cb622d9f68a8a92cfc6037
joaocaetanosilva <joaocaetanosilva1234@gmail.com>
2021-12-29 21:57:10 -0300

    Atualização

--- aula6.py
+++ aula6.py
@@ -1,6 +1,12 @@
 conjunto1 = {1, 2, 3, 4, 5}
 conjunto2 = {5, 6, 7, 8, 9}
 conjunto_uniao = conjunto1.union(conjunto2)
-print(conjunto_uniao)
+print('União: {}'.format(conjunto_uniao))
+conjunto_interseccao = conjunto1.intersection(conjunto2)
+print('Interseccao: {}'.format(conjunto_interseccao))
+conjunto_diferenca1 = conjunto1.difference(conjunto2)
+conjunto_diferenca2 =conjunto2.difference(conjunto1)
+print('Diferença entre o conjunto 1 e o conjunto 2: {}'.format(conjunto_diferenca1))
+print('Diferença entre o conjunto 2 e o conjunto 1: {}'.format(conjunto_diferenca2))
 
 # Obs: 0 (+) não funciona pois é {}.
