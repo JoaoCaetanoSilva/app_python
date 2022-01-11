2b4a14c1c9a97890bd210ea2198286657065df3e
joaocaetanosilva <joaocaetanosilva1234@gmail.com>
2022-01-11 18:40:04 -0300

    Atualização 2022

--- aula7.py
+++ aula7.py
@@ -1,18 +1,29 @@
-codigo = input('Codificar: ')
+numero1 = int(input('Entre com o numero 1: '))
+numero2 = int(input('Entre com o numero 2: '))
 
-a = 1
-b = 2
-c = 3
-d = 4
-e = 5
-f = 6
-g = 7
-h = 8
-i = 9
+class Calculadora:
+    def __init__(self, num1, num2):
+        self.valor_a = num1
+        self.valor_b = num2
 
-lista = (a, b, c, d, e, f, g, h, i)
+    def soma(self):
+        return self.valor_a + self.valor_b
 
-if codigo in lista:
-    print(codigo)
+    def subtracao(self):
+        return self.valor_a - self.valor_b
+
+    def multiplicacao(self):
+        return self.valor_a * self.valor_b
+
+    def divisao(self):
+        return self.valor_a / self.valor_b
+
+if __name__=='__main__':
+    calculadora = Calculadora(numero1, numero1)
+    print('Soma: {}'.format(calculadora.soma()))
+    print('Subtracao: {}'.format(calculadora.subtracao()))
+    print('Multiplicacao: {}'.format(calculadora.multiplicacao()))
+    print('Divisao: {}'.format(calculadora.divisao()))
+
 
 
