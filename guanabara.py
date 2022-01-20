8f8e594a17fe8563d27f5d746eb20d54c00d9515
joaocaetanosilva <joaocaetanosilva1234@gmail.com>
2022-01-20 16:17:26 -0300

    Atualização 2022 2.0

--- guanabara.py
+++ guanabara.py
@@ -13,5 +13,12 @@
 # print('A soma entre {} e {} é de: {}'.format(n1, n2, z))
 # # or
 # print('A soma entre', n1, 'e', n2, 'é de:', z)
-
-
+digite = input('Digite qualquer coisa: ')
+print('Qual o tipo??\nR:', type(digite))
+print('Está em branco??\nR:', digite.isspace())
+print('São numeros?\nR:', digite.isnumeric())
+print('São apenas letras?\nR:', digite.isalpha())
+print('São apenas numeros?\nR:', digite.isalnum())
+print('Está apenas em maiusculo?\nR:', digite.isupper())
+print('Esta apenas em minusculo?\nR:', digite.islower())
+print('Esta mesclada?\nR:', digite.istitle())
---
+++ projeto3.py
@@ -0,0 +1,3 @@
+palavra = ""
+codificar = [ord(x) - 97 for x in palavra]
+print(codificar)
