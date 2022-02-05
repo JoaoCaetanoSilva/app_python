928022748e7c2867631a33963579f51a3f2b9a9c
joaocaetanosilva <joaocaetanosilva1234@gmail.com>
2022-02-05 18:21:54 -0300

    Atualização 2022 3.0

--- aula4.py
+++ aula4.py
@@ -1,22 +1,22 @@
-# nota = int(input('Entre com a nota: '))
-# while nota > 10:
-#     nota = int(input('(Nota invalida!) \nNota correta: '))
+nota = int(input('Entre com a nota: '))
+while nota > 10:
+    nota = int(input('(Nota invalida!) \nNota correta: '))
+
+a = nota
+while a < 10:
+    print(a)
+    a += 1
+
+# a = int(input('Entre com um valor: '))
 #
-# a = nota
-# while a < 10:
-#     print(a)
-#     a += 1
-
-a = int(input('Entre com um valor: '))
-
-div = 0
-for x in range(1, a+1):
-        resto = a % x
-        if resto == 0:
-            div = div + 1
-
-if div == 2:
-        print('Numero {} é primo'.format(a))
-else:
-    print('Numero {} não é primo'.format(a))
+# div = 0
+# for x in range(1, a+1):
+#         resto = a % x
+#         if resto == 0:
+#             div = div + 1
+#
+# if div == 2:
+#         print('Numero {} é primo'.format(a))
+# else:
+#     print('Numero {} não é primo'.format(a))
 
--- nada_a_esperar.py
+++ nada_a_esperar.py
@@ -3,59 +3,68 @@ lista1 = []
 lista1.append(nomes)
 lista1 = nomes.split()
 
-def atualizar_arquivo1(texto):
-    arquivo = open('cliente.txt', 'w')
-    arquivo.write(texto)
-    atualizar_arquivo1(nomes)
-    arquivo.close()
+arquivo = open('cliente.txt', 'w')
+arquivo.write(nomes)
+arquivo.close()
 
-endereco = input('Endereco dos cliente: ')
+modificar = int(input('Deseja adicionar o nome de um novo cliente, exclui-lo ou altera-lo? Se sua resposta for sim. DIGITE 1. Se nao DIGITE 2: '))
+if modificar == 1:
+    confirmado = int(input('Para adicionar o nome de um novo cliente digite: 2 \nPara excluir o nome do cliente digite: 3 \nPara altera-lo digite: 4: \nOpção: '))
+
+    if confirmado == 2:
+        adicionar1 = input('Nome do novo cliente: ')
+        arquivo = open('cliente.txt', 'a')
+        arquivo.write(' ' + adicionar1)
+        arquivo.close()
+
+    if confirmado == 3:
+        excluir1 = input('Nome que deseja excluir: ')
+        lista1.remove(excluir1)
+        separar1 = ' '.join(lista1)
+        arquivo = open('cliente.txt', 'w')
+        arquivo.write(separar1)
+        arquivo.close()
+
+    if confirmado == 4:
+        alterar1 = input('Nome que deseja alterar: ')
+        novo1 = input('Novo nome: ')
+        for index, palavra in enumerate(lista1):
+            if palavra == alterar1:
+                lista1[index] = novo1
+        separar2 = ' '.join(lista1)
+        arquivo = open('cliente.txt', 'w')
+        arquivo.write(separar2)
+#/
+x = len(lista1)
+y = 0
+while y < x:
+    y += 1
+
+for z in lista1:
+    endereco = input('Endereco de ' + lista1[y] + ': ')
+
 lista2 = []
 lista2.append(endereco)
-lista2 = endereco.split()
+lista2 = (endereco).split()
 
-def atualizar_arquivo2(texto):
-    arquivo = open('endereco.txt', 'w')
-    arquivo.write(texto)
-    atualizar_arquivo2(endereco)
-    arquivo.close()
+mudar2 = int(input('Deseja adicionar o endereco de um novo cliente, exclui-lo ou altera-lo? Se sua resposta for sim. DIGITE 1. Se nao DIGITE 2: '))
+if mudar2 == 1:
+    confirmado2 = int(input('Para adicionar o endereco de um novo cliente digite: 2 \nPara excluir o endereco do cliente digite: 3 \nPara altera-lo digite: 4: \nOpção: '))
 
-codigo = input('Codigo dos clientes: ')
-lista3 = []
-lista3.append(codigo)
-lista3 = codigo.split()
+    if confirmado2 == 2:
+        adicionar2 = input('Endereco do novo cliente: ')
+        lista2.append(adicionar2)
+        print(lista2)
 
-def atualizar_arquivo2(texto):
-    arquivo = open('codigo.txt', 'w')
-    arquivo.write(texto)
-    atualizar_arquivo2(codigo)
-    arquivo.close()
+    if confirmado2 == 3:
+        excluir2 = input('Endereco que deseja excluir: ')
+        lista2.remove(excluir2)
+        print(lista2)
 
-class Modificacao_Cliente:
-    modificar = int(input('Deseja adicionar o nome de um novo cliente, exclui-lo ou altera-lo? Se sua resposta for sim. DIGITE 1. Se nao DIGITE 2: '))
-    if modificar == 1:
-        confirmado = int(input('Para adicionar o nome de um novo cliente digite: 2 \nPara excluir o nome do cliente digite: 3 \nPara altera-lo digite: 4: \nOpção: '))
-
-        if confirmado == 2:
-            adicionar1 = input('Nome do novo cliente: ')
-            from nada_a_esperar import atualizar_arquivo1
-            atualizar_arquivo1.append(adicionar1)
-            print(lista1)
-
-        if confirmado == 3:
-            excluir1 = input('Nome que deseja excluir: ')
-            from nada_a_esperar import atualizar_arquivo1
-            atualizar_arquivo1.remove(excluir1)
-            print(lista1)
-
-        if confirmado == 4:
-            alterar1 = input('Nome que deseja alterar: ')
-            novo1 = input('Novo nome: ')
-            from nada_a_esperar import atualizar_arquivo1
-            for index, palavra in enumerate(atualizar_arquivo1):
-                if palavra == alterar1:
-                    lista1[index] = novo1
-            print(lista1)
-
-        else:
-            ...
+    if confirmado2 == 4:
+        alterar2 = input('Endereco que deseja alterar: ')
+        novo2 = input('Novo endereco: ')
+        for index, palavra in enumerate(lista2):
+            if palavra == alterar2:
+                lista2[index] = novo2
+        print(lista2)
