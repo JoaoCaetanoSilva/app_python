74efa3033f540d0e59f616a4445c3b4ca54e7855
joaocaetanosilva <joaocaetanosilva1234@gmail.com>
2022-04-11 14:55:49 -0300

    9.0

--- agenda/agenda/urls.py
+++ agenda/agenda/urls.py
@@ -1,4 +1,4 @@
-"""django_1 URL Configuration
+"""agenda URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
@@ -15,11 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/<nome>/<int:idade>/', views.hello),
-    path('soma/<int:num1>/<int:num2>/', views.soma)
-
 ]
--- core/views.py
+++ core/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render, HttpResponse
 
 def hello(request, nome, idade):
     return HttpResponse('<h1>Hello {} de {} anos!</h1>'.format(nome, idade))
+
+def soma(request, num1, num2):
+    soma = num1 + num2
+    return HttpResponse('A soma entre {} e {} é: {}'.format(num1, num2, soma))
