cf1736e0547a0371bbe5520973f6c4ff5240d92e
joaocaetanosilva <joaocaetanosilva1234@gmail.com>
2022-04-12 22:02:03 -0300

    10.0

--- agenda/core/models.py
+++ agenda/core/models.py
@@ -2,4 +2,13 @@ from django.db import models
 
 # Create your models here.
 
-class Evento
+class Evento(models.Model):
+    titulo = models.CharField(max_length=100)
+    descricao = models.IntegerField(blank=True, null=True)
+    data_titulo = models.DateTimeField()
+    data_criacao = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'Tabela'
+
+
