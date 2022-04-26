e6f9e96f43b9ab0cc38b6fca71ad64874d54c2c1
joaocaetanosilva <joaocaetanosilva1234@gmail.com>
2022-04-25 21:17:34 -0300

    10.0

--- agenda/core/admin.py
+++ agenda/core/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from core.models import Evento
+from .models import Evento
 
 # Register your models here.
 
-admin.site.register(Evento)
+class EventoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'data_titulo', 'data_criacao')
+
+admin.site.register(Evento, EventoAdmin)
